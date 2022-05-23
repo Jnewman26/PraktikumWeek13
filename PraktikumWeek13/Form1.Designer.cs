95b458c0968f0f5e15210f9f2a07b552a38172fa
James Newman <jnewman01@student.ciputra.ac.id>
2022-05-23 13:47:07 +0700

    Add project files.

---
+++ PraktikumWeek13/Form1.Designer.cs
@@ -0,0 +1,280 @@
+﻿namespace PraktikumWeek13
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonFirst = new System.Windows.Forms.Button();
+            this.buttonPrev = new System.Windows.Forms.Button();
+            this.buttonNext = new System.Windows.Forms.Button();
+            this.buttonLast = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBoxID = new System.Windows.Forms.TextBox();
+            this.textBoxName = new System.Windows.Forms.TextBox();
+            this.comboBoxTeam = new System.Windows.Forms.ComboBox();
+            this.numericUpDownTeamNumber = new System.Windows.Forms.NumericUpDown();
+            this.buttonSave = new System.Windows.Forms.Button();
+            this.buttonCancel = new System.Windows.Forms.Button();
+            this.comboBoxNationality = new System.Windows.Forms.ComboBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.labelStatus = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownTeamNumber)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonFirst
+            // 
+            this.buttonFirst.Location = new System.Drawing.Point(170, 36);
+            this.buttonFirst.Name = "buttonFirst";
+            this.buttonFirst.Size = new System.Drawing.Size(114, 86);
+            this.buttonFirst.TabIndex = 0;
+            this.buttonFirst.Text = "<<";
+            this.buttonFirst.UseVisualStyleBackColor = true;
+            this.buttonFirst.Click += new System.EventHandler(this.buttonFirst_Click);
+            // 
+            // buttonPrev
+            // 
+            this.buttonPrev.Location = new System.Drawing.Point(336, 36);
+            this.buttonPrev.Name = "buttonPrev";
+            this.buttonPrev.Size = new System.Drawing.Size(114, 86);
+            this.buttonPrev.TabIndex = 0;
+            this.buttonPrev.Text = "<";
+            this.buttonPrev.UseVisualStyleBackColor = true;
+            this.buttonPrev.Click += new System.EventHandler(this.buttonPrev_Click);
+            // 
+            // buttonNext
+            // 
+            this.buttonNext.Location = new System.Drawing.Point(508, 36);
+            this.buttonNext.Name = "buttonNext";
+            this.buttonNext.Size = new System.Drawing.Size(114, 86);
+            this.buttonNext.TabIndex = 0;
+            this.buttonNext.Text = ">";
+            this.buttonNext.UseVisualStyleBackColor = true;
+            this.buttonNext.Click += new System.EventHandler(this.buttonNext_Click);
+            // 
+            // buttonLast
+            // 
+            this.buttonLast.Location = new System.Drawing.Point(666, 36);
+            this.buttonLast.Name = "buttonLast";
+            this.buttonLast.Size = new System.Drawing.Size(114, 86);
+            this.buttonLast.TabIndex = 0;
+            this.buttonLast.Text = ">>";
+            this.buttonLast.UseVisualStyleBackColor = true;
+            this.buttonLast.Click += new System.EventHandler(this.buttonLast_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(215, 190);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(99, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Player ID";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(215, 238);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(135, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Player Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(215, 292);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(101, 25);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "BirthDate";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(215, 342);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(113, 25);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Nationality";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(215, 398);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(66, 25);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "Team";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(215, 450);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(147, 25);
+            this.label6.TabIndex = 1;
+            this.label6.Text = "Team Number";
+            // 
+            // textBoxID
+            // 
+            this.textBoxID.Location = new System.Drawing.Point(419, 190);
+            this.textBoxID.Name = "textBoxID";
+            this.textBoxID.Size = new System.Drawing.Size(194, 31);
+            this.textBoxID.TabIndex = 2;
+            // 
+            // textBoxName
+            // 
+            this.textBoxName.Location = new System.Drawing.Point(419, 235);
+            this.textBoxName.Name = "textBoxName";
+            this.textBoxName.Size = new System.Drawing.Size(286, 31);
+            this.textBoxName.TabIndex = 2;
+            // 
+            // comboBoxTeam
+            // 
+            this.comboBoxTeam.FormattingEnabled = true;
+            this.comboBoxTeam.Location = new System.Drawing.Point(419, 394);
+            this.comboBoxTeam.Name = "comboBoxTeam";
+            this.comboBoxTeam.Size = new System.Drawing.Size(286, 33);
+            this.comboBoxTeam.TabIndex = 3;
+            // 
+            // numericUpDownTeamNumber
+            // 
+            this.numericUpDownTeamNumber.Location = new System.Drawing.Point(419, 448);
+            this.numericUpDownTeamNumber.Name = "numericUpDownTeamNumber";
+            this.numericUpDownTeamNumber.Size = new System.Drawing.Size(194, 31);
+            this.numericUpDownTeamNumber.TabIndex = 4;
+            this.numericUpDownTeamNumber.ValueChanged += new System.EventHandler(this.numericUpDownTeamNumber_ValueChanged);
+            // 
+            // buttonSave
+            // 
+            this.buttonSave.Location = new System.Drawing.Point(220, 559);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(186, 55);
+            this.buttonSave.TabIndex = 5;
+            this.buttonSave.Text = "Save";
+            this.buttonSave.UseVisualStyleBackColor = true;
+            this.buttonSave.Click += new System.EventHandler(this.buttonSave_Click);
+            // 
+            // buttonCancel
+            // 
+            this.buttonCancel.Location = new System.Drawing.Point(568, 559);
+            this.buttonCancel.Name = "buttonCancel";
+            this.buttonCancel.Size = new System.Drawing.Size(186, 55);
+            this.buttonCancel.TabIndex = 5;
+            this.buttonCancel.Text = "Cancel";
+            this.buttonCancel.UseVisualStyleBackColor = true;
+            this.buttonCancel.Click += new System.EventHandler(this.buttonCancel_Click);
+            // 
+            // comboBoxNationality
+            // 
+            this.comboBoxNationality.FormattingEnabled = true;
+            this.comboBoxNationality.Location = new System.Drawing.Point(419, 339);
+            this.comboBoxNationality.Name = "comboBoxNationality";
+            this.comboBoxNationality.Size = new System.Drawing.Size(286, 33);
+            this.comboBoxNationality.TabIndex = 3;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dateTimePicker1.Location = new System.Drawing.Point(419, 287);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(361, 31);
+            this.dateTimePicker1.TabIndex = 6;
+            this.dateTimePicker1.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
+            // 
+            // labelStatus
+            // 
+            this.labelStatus.AutoSize = true;
+            this.labelStatus.Location = new System.Drawing.Point(635, 453);
+            this.labelStatus.Name = "labelStatus";
+            this.labelStatus.Size = new System.Drawing.Size(19, 25);
+            this.labelStatus.TabIndex = 7;
+            this.labelStatus.Text = "-";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(956, 672);
+            this.Controls.Add(this.labelStatus);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.buttonCancel);
+            this.Controls.Add(this.buttonSave);
+            this.Controls.Add(this.numericUpDownTeamNumber);
+            this.Controls.Add(this.comboBoxNationality);
+            this.Controls.Add(this.comboBoxTeam);
+            this.Controls.Add(this.textBoxName);
+            this.Controls.Add(this.textBoxID);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonLast);
+            this.Controls.Add(this.buttonNext);
+            this.Controls.Add(this.buttonPrev);
+            this.Controls.Add(this.buttonFirst);
+            this.Margin = new System.Windows.Forms.Padding(6);
+            this.Name = "Form1";
+            this.Text = "Player";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownTeamNumber)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonFirst;
+        private System.Windows.Forms.Button buttonPrev;
+        private System.Windows.Forms.Button buttonNext;
+        private System.Windows.Forms.Button buttonLast;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBoxID;
+        private System.Windows.Forms.TextBox textBoxName;
+        private System.Windows.Forms.ComboBox comboBoxTeam;
+        private System.Windows.Forms.NumericUpDown numericUpDownTeamNumber;
+        private System.Windows.Forms.Button buttonSave;
+        private System.Windows.Forms.Button buttonCancel;
+        private System.Windows.Forms.ComboBox comboBoxNationality;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label labelStatus;
+    }
+}
+
---
+++ PraktikumWeek13/Form1.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace PraktikumWeek13
+{
+    public partial class Form1 : Form
+    {
+        public static string sqlConnection = "server=139.255.11.84;uid=student;pwd=isbmantap;database=premier_league";
+        public MySqlConnection sqlConnect = new MySqlConnection(sqlConnection);
+        public MySqlCommand sqlCommand;
+        public MySqlDataAdapter sqlAdapter;
+        string sqlQuery;
+        int counter = 0;
+        string temp = "";
+
+        public Form1()
+        {
+            InitializeComponent();
+            dateTimePicker1.Format = DateTimePickerFormat.Custom;
+            dateTimePicker1.CustomFormat = "dddd ,MMMM dd, yyyy";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            sqlConnect.Open();
+            sqlQuery = "SELECT `player_id`, `team_number`, `player_name`, `nation`, `birthdate`,`team_name`, IF(status = 1, 'Available', 'Not Available') as status FROM team t, player_james p, nationality n WHERE p.nationality_id = n.nationality_id AND t.team_id = p.team_id";
+            sqlCommand = new MySqlCommand(sqlQuery, sqlConnect);
+            sqlAdapter = new MySqlDataAdapter(sqlCommand);
+            DataTable dt = new DataTable();
+            sqlAdapter.Fill(dt);
+            textBoxID.Text = dt.Rows[0][0].ToString();
+            textBoxName.Text = dt.Rows[0][2].ToString();
+            dateTimePicker1.Text = dt.Rows[0][4].ToString();
+            numericUpDownTeamNumber.Text = dt.Rows[counter][1].ToString(); 
+            labelStatus.Text = dt.Rows[0][6].ToString();
+
+            DataTable nationality = new DataTable();
+            sqlQuery = "SELECT n.nation, n.nationality_id FROM nationality n";
+            sqlCommand = new MySqlCommand(sqlQuery, sqlConnect);
+            sqlAdapter = new MySqlDataAdapter(sqlCommand);
+            sqlAdapter.Fill(nationality);
+            comboBoxNationality.DataSource = nationality;
+            comboBoxNationality.ValueMember = "nation";
+
+            DataTable team = new DataTable();
+            sqlQuery = "SELECT t.team_name, t.team_id FROM team t";
+            sqlCommand = new MySqlCommand(sqlQuery, sqlConnect);
+            sqlAdapter = new MySqlDataAdapter(sqlCommand);
+            sqlAdapter.Fill(team);
+            comboBoxTeam.DataSource = team;
+            comboBoxTeam.ValueMember = "team_name";
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void buttonFirst_Click(object sender, EventArgs e)
+        {
+            counter = 0;
+            sqlQuery = "SELECT `player_id`, `team_number`, `player_name`, `nation`, `birthdate`,`team_name`, IF(status = 1, 'Available', 'Not Available') as status, n.nation, n.nationality_id, t.team_name, t.team_id FROM team t, player_james p, nationality n WHERE p.nationality_id = n.nationality_id AND t.team_id = p.team_id";
+            sqlCommand = new MySqlCommand(sqlQuery, sqlConnect);
+            sqlAdapter = new MySqlDataAdapter(sqlCommand);
+            DataTable dt = new DataTable();
+            sqlAdapter.Fill(dt);
+            textBoxID.Text = dt.Rows[counter][0].ToString();
+            textBoxName.Text = dt.Rows[counter][2].ToString();
+            dateTimePicker1.Text = dt.Rows[counter][4].ToString();
+            numericUpDownTeamNumber.Text = dt.Rows[counter][1].ToString();
+            labelStatus.Text = dt.Rows[counter][6].ToString();
+
+            comboBoxTeam.SelectedValue = dt.Rows[counter][5].ToString();
+            comboBoxNationality.SelectedValue = dt.Rows[counter][7].ToString();
+        }
+
+        private void buttonPrev_Click(object sender, EventArgs e)
+        {
+            if (counter <= 0)
+            {
+                MessageBox.Show("Sudah data pertama");
+            }
+            else
+            {
+                counter--;
+                sqlQuery = "SELECT `player_id`, `team_number`, `player_name`, `nation`, `birthdate`,`team_name`, IF(status = 1, 'Available', 'Not Available') as status, n.nation, n.nationality_id, t.team_name, t.team_id FROM team t, player_james p, nationality n WHERE p.nationality_id = n.nationality_id AND t.team_id = p.team_id";
+                sqlCommand = new MySqlCommand(sqlQuery, sqlConnect);
+                sqlAdapter = new MySqlDataAdapter(sqlCommand);
+                DataTable dt = new DataTable();
+                sqlAdapter.Fill(dt);
+                textBoxID.Text = dt.Rows[counter][0].ToString();
+                textBoxName.Text = dt.Rows[counter][2].ToString();
+                dateTimePicker1.Text = dt.Rows[counter][4].ToString();
+                numericUpDownTeamNumber.Text = dt.Rows[counter][1].ToString();
+                labelStatus.Text = dt.Rows[counter][6].ToString();
+
+                comboBoxTeam.SelectedValue = dt.Rows[counter][5].ToString();
+                comboBoxNationality.SelectedValue = dt.Rows[counter][7].ToString();
+            }
+        }
+
+        private void buttonLast_Click(object sender, EventArgs e)
+        {
+            counter = 858;
+            sqlQuery = "SELECT `player_id`, `team_number`, `player_name`, `nation`, `birthdate`,`team_name`, IF(status = 1, 'Available', 'Not Available') as status, n.nation, n.nationality_id, t.team_name, t.team_id FROM team t, player_james p, nationality n WHERE p.nationality_id = n.nationality_id AND t.team_id = p.team_id";
+            sqlCommand = new MySqlCommand(sqlQuery, sqlConnect);
+            sqlAdapter = new MySqlDataAdapter(sqlCommand);
+            DataTable dt = new DataTable();
+            sqlAdapter.Fill(dt);
+            textBoxID.Text = dt.Rows[counter][0].ToString();
+            textBoxName.Text = dt.Rows[counter][2].ToString();
+            dateTimePicker1.Text = dt.Rows[counter][4].ToString();
+            numericUpDownTeamNumber.Text = dt.Rows[counter][1].ToString();
+            labelStatus.Text = dt.Rows[counter][6].ToString();
+
+            comboBoxTeam.SelectedValue = dt.Rows[counter][5].ToString();
+            comboBoxNationality.SelectedValue = dt.Rows[counter][7].ToString();
+        }
+
+        private void buttonNext_Click(object sender, EventArgs e)
+        {
+            if (counter >= 858)
+            {
+                MessageBox.Show("Sudah data terakhir");
+            }
+            else
+            {
+                counter++;
+                sqlQuery = "SELECT `player_id`, `team_number`, `player_name`, `nation`, `birthdate`,`team_name`, IF(status = 1, 'Available', 'Not Available') as status, n.nation, n.nationality_id, t.team_name, t.team_id FROM team t, player_james p, nationality n WHERE p.nationality_id = n.nationality_id AND t.team_id = p.team_id";
+                sqlCommand = new MySqlCommand(sqlQuery, sqlConnect);
+                sqlAdapter = new MySqlDataAdapter(sqlCommand);
+                DataTable dt = new DataTable();
+                sqlAdapter.Fill(dt);
+                textBoxID.Text = dt.Rows[counter][0].ToString();
+                textBoxName.Text = dt.Rows[counter][2].ToString();
+                dateTimePicker1.Text = dt.Rows[counter][4].ToString();
+                numericUpDownTeamNumber.Text = dt.Rows[counter][1].ToString();
+                labelStatus.Text = dt.Rows[counter][6].ToString();
+
+                comboBoxTeam.SelectedValue = dt.Rows[counter][5].ToString();
+                comboBoxNationality.SelectedValue = dt.Rows[counter][7].ToString();
+            }
+        }
+
+        private void buttonCancel_Click(object sender, EventArgs e)
+        {
+            System.Windows.Forms.Application.Exit();
+        }
+
+        private void buttonSave_Click(object sender, EventArgs e)
+        {
+            if (labelStatus.Text == "Not Available")
+            {
+                MessageBox.Show("Team Number sudah dipakai");
+            }
+            else
+            {
+                sqlQuery = "UPDATE  player_name = '" + textBoxName.Text + "', nationality_id = '" + comboBoxNationality.SelectedValue.ToString() + "', birthdate = '" + dateTimePicker1.Value.ToString("yyyMMdd") + "', team_id = '" + comboBoxTeam.SelectedValue.ToString() + "' where player_id = '" + textBoxID.Text + "'";
+
+                sqlCommand = new MySqlCommand(sqlQuery, sqlConnect);
+
+                MessageBox.Show("Data Player bernama " + textBoxName.Text + " berhasil diupdate.");
+                sqlConnect.Close();
+            }
+        }
+
+        private void numericUpDownTeamNumber_ValueChanged(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
